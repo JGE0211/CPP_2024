52f2ed9468d6dcdb5e5b75bef6bd5f67b6acfe3a
JGE0211 <ke8650@gmail.com>
2024-11-13 10:28:32 +0900

    Add files via upload

---
+++ 004_loop/004_loop.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a[] = { 1,2,3,4,5,6,7,8,9,10 };
+
+	// 범위 기반 for문
+	for (int i : a)    // 배열의 개수를 몰라도 됨 ( i는 배열에 저장된 값(원소)을 찍음)
+		cout << i*i << ' ';
+	cout << endl;
+}
---
+++ 005_swap/005_swap.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+void swap(int* x, int* y)
+{
+	int tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+void swap(int& x, int& y) // 레퍼런스 변수 사용(레퍼런스 변수는 보내주는 것의 다른 이름과도 같아서 여기서 바뀌면 보내주는 곳에서도 바뀜) 
+///--- swap 두 개 해도 에러 안 되는 이유 : cpp에서는 함수 중복이 가능함( 하나으ㅣ 함수이름에 여러 개 가능;; 매개변수에 맞는 함수를 찾아감 )
+{
+	int t = x;
+	x = y;
+	y = t;
+}
+
+void swap(double& x, double& y) 
+{
+	double t = x;
+	x = y;
+	y = t;
+}
+int main()
+{
+	int a = 4;
+	int b = 5;
+
+	double x = 1.1234;
+	double y = 5.4321;
+
+
+	//swap(&a, &b);
+	swap(a, b);
+	swap(x, y);
+
+	cout << a << ' ' << b << endl;
+	cout << x << ' ' << y << endl;
+}
